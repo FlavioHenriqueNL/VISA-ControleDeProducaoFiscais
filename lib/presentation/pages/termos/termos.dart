1ed1a58dc208ab0f1685015a109815145b1b4d78
Flávio Henrique <f.henrique.n.l@gmail.com>
2025-09-01 12:51:08 -0300

    Criação da pagina de parer sanitário

--- lib/presentation/pages/termos/termos.dart
+++ lib/presentation/pages/termos/termos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:visa_arapiraca_app/presentation/widgets/scrollable_page.dart';
 
 class TermosPage extends StatefulWidget {
@@ -8,6 +9,15 @@ class TermosPage extends StatefulWidget {
   State<TermosPage> createState() => _TermosPageState();
 }
 
+final List<List<String>> termosLista = [
+  ['Parecer Sanitário', 'parecer-sanitario'],
+  ['Termo de Inspeção', 'termo-inspecao'],
+  ['Notificação', 'notificacao'],
+  ['Auto de Infração', 'auto-infracao'],
+  ['Termo de Apreensão', 'termo-apreensao'],
+  ['Interdição', 'interdicao'],
+];
+
 class _TermosPageState extends State<TermosPage> {
   @override
   Widget build(BuildContext context) {
@@ -15,8 +25,39 @@ class _TermosPageState extends State<TermosPage> {
       child: Container(
         padding: const EdgeInsets.all(25.0),
         color: Colors.white,
-        child: Column(children: [Text("Termos e Pareceres Sanitários")]),
+        child: Column(
+          children: [
+            const Text(
+              "Termos e Pareceres",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Wrap(
+              spacing: 12, // espaço horizontal entre os cards
+              runSpacing: 12, // espaço vertical entre as linhas
+              children: termosLista.map((termo) {
+                return ElevatedButton(
+                  style: botaoTermos,
+                  onPressed: () {
+                    context.go('/termos/${termo[1]}');
+                    Navigator.pushNamed(context, '/termos/${termo[1]}');
+                  },
+                  child: Text(termo[0], textAlign: TextAlign.center),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+final ButtonStyle botaoTermos = ElevatedButton.styleFrom(
+  backgroundColor: Colors.blue,
+  foregroundColor: Colors.white,
+  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+  textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+  minimumSize: Size(200, 120),
+);
--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -7,21 +7,38 @@ import 'package:visa_arapiraca_app/presentation/pages/home/home.dart';
 import 'package:visa_arapiraca_app/presentation/pages/login/login.dart';
 import 'package:visa_arapiraca_app/presentation/pages/producao/producao_diaria.dart';
 import 'package:visa_arapiraca_app/presentation/pages/recuperar_senha/recuperar_senha.dart';
+import 'package:visa_arapiraca_app/presentation/pages/termos/parecer-sanitario.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/login',
   routes: [
     GoRoute(path: '/login', builder: (context, state) => Login()),
-    GoRoute(path: '/recuperar-acesso', builder: (context, state) => RecuperarSenha()),
+    GoRoute(
+      path: '/recuperar-acesso',
+      builder: (context, state) => RecuperarSenha(),
+    ),
     ShellRoute(
       builder: (context, state, child) => Dashboard(content: child),
       routes: [
         GoRoute(path: '/dashboard', builder: (context, state) => Home()),
         GoRoute(path: '/termos', builder: (context, state) => TermosPage()),
+        GoRoute(
+          path: '/termos/parecer-sanitario',
+          builder: (context, state) => ParecerSanitarioPage(),
+        ),
         GoRoute(path: '/producao', builder: (context, state) => ProducaoPage()),
-        GoRoute(path: '/estabelecimentos', builder: (context, state) => Center(child: Text('Página em construção'))),
+        GoRoute(
+          path: '/estabelecimentos',
+          builder: (context, state) =>
+              Center(child: Text('Página em construção')),
+        ),
         GoRoute(path: '/lista-cnaes', builder: (context, state) => CnaesPage()),
-        GoRoute(path: '/notificacoes', builder: (context, state) => Center(child: Text('Página em construção'))),
-      ])
+        GoRoute(
+          path: '/notificacoes',
+          builder: (context, state) =>
+              Center(child: Text('Página em construção')),
+        ),
+      ],
+    ),
   ],
 );
